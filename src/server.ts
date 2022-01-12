cf8859b0c437344daae87fee35392d06c9b6e9f9
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2022-01-11 23:44:34 -0300

    Add env vars

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
 import express, { Request, Response } from 'express';
 import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const router = express.Router();
-
-app.use(cors());
+app
+  .use(express.json())
+  .use(cors())
+  .use('/', router);
 
 router.get('/', (req:Request, res:Response) => res.status(200).send('The app in on!'));
 
-app.listen(process.env.PORT || 3030);
+app.listen(process.env.PORT);
