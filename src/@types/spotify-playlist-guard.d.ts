18b7e38c09b9af412f89068c7d882c9466a7c088
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2022-03-02 23:44:33 -0300

    Integrate the app with the API
    -Retrieve playlist and user data from the API;
    -Use playlist data to make requests to the spotify API

---
+++ src/@types/spotify-playlist-guard.d.ts
@@ -0,0 +1,34 @@
+export interface User {
+  country: string;
+  display_name: string;
+  email: string;
+  external_url: string;
+  followers: number;
+  href: string;
+  id: string;
+  images: string[];
+  product: string;
+  type: string;
+  uri: string;
+  playlists: [];
+  accessToken: string;
+  refreshToken: string;
+  expiresAt: Date;
+}
+
+export interface Playlist {
+  collaborative: boolean;
+  description: string;
+  external_url: string;
+  followers: number;
+  href: string;
+  id: string;
+  name: string;
+  owner: User;
+  public: boolean;
+  snapshot_id: string;
+  tracks: string[];
+  uri: string;
+  active: boolean;
+  allowed_userIds: string[];
+}
--- src/server.ts
+++ src/server.ts
@@ -1,64 +1,100 @@
-import express, { Request, Response } from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import cron from 'node-cron';
-import SpotifyWebApi from 'spotify-web-api-node';
+import express, { Request, Response } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import cron from "node-cron";
+import SpotifyWebApi from "spotify-web-api-node";
+import axios from "axios";
+import { Playlist } from "./@types/spotify-playlist-guard";
 
 dotenv.config();
 
 const app = express();
 const router = express.Router();
-const spotifyRefreshToken = process.env.SPOTIFY_REFRESH_TOKEN as string;
 
-app
-  .use(express.json())
-  .use(cors())
-  .use('/', router);
+app.use(express.json()).use(cors()).use("/", router);
 
-const botCronJob = cron.schedule('0 * * * * *', async () => {
-  const allowedUsersIDs = ['31ronlkcdosn3x3kkuvez6vo2jyi'];
-  const guardedPlaylistID = '5wkdAzv8ZArH5cyvXQTRYe';
-  const tracksToRemove: any = [];
-  const spotifyApiClient = new SpotifyWebApi(
-    {
-      clientId: process.env.SPOTIFY_CLIENT_ID,
-      clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-      redirectUri: `http://localhost:${process.env.PORT}/`,
-    },
-  );
-
-  spotifyApiClient.setRefreshToken(spotifyRefreshToken);
-
-  await spotifyApiClient.refreshAccessToken().then((data) => {
-    spotifyApiClient.setAccessToken(data.body.access_token);
-  });
-
-  await spotifyApiClient.getPlaylist(guardedPlaylistID).then(
-    (data) => {
-      const tracksArr = data.body.tracks.items;
-
-      tracksArr.forEach((track) => {
-        const userId = track.added_by.id;
-
-        if (!allowedUsersIDs.includes(userId)) {
-          const trackId = track.track.id;
-          tracksToRemove.push({ uri: `spotify:track:${trackId}` });
-        }
-      });
-    },
-    (err) => {
-      throw err;
-    },
-  );
-
-  await spotifyApiClient.removeTracksFromPlaylist(guardedPlaylistID, tracksToRemove);
-}, {
-  scheduled: true,
-  timezone: 'America/Sao_Paulo',
+const axiosInstace = axios.create({
+  baseURL: process.env.API_URL,
+  timeout: 5000,
+  headers: { Authorization: process.env.API_CLIENT_KEY },
+  params: { id: process.env.API_CLIENT_ID },
 });
 
-router.get('/', async (req:Request, res:Response) => {
-  res.status(200).send('The Guard Bot is on!');
+const spotifyApiClient = new SpotifyWebApi({
+  clientId: process.env.SPOTIFY_CLIENT_ID,
+  clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+  redirectUri: `http://localhost:${process.env.PORT}/`,
+});
+
+const botCronJob = cron.schedule(
+  "0 * * * * *",
+  async () => {
+    const playlists: Playlist[] = (
+      await axiosInstace.get("/playlists/findAll/active").catch((error) => {
+        throw error;
+      })
+    ).data;
+
+    for (let i = 0; i < playlists.length; i++) {
+      const playlist = playlists[i];
+      const { id, allowed_userIds, owner, snapshot_id } = playlist;
+      const { refreshToken } = owner;
+      const tracksToRemove: any[] = [];
+      const newTracks: string[] = [];
+      let currSnapshotId: string;
+
+      spotifyApiClient.setRefreshToken(refreshToken);
+
+      await spotifyApiClient.refreshAccessToken().then((data) => {
+        spotifyApiClient.setAccessToken(data.body.access_token);
+      });
+
+      await spotifyApiClient.getPlaylist(id).then(
+        (data) => {
+          const tracks = data.body.tracks.items;
+          currSnapshotId = data.body.snapshot_id;
+
+          if (currSnapshotId !== snapshot_id) {
+            tracks.forEach((track) => {
+              const userId = track.added_by.id;
+              const trackId = track.track.id;
+
+              if (!allowed_userIds.includes(userId)) {
+                tracksToRemove.push({ uri: `spotify:track:${trackId}` });
+                return;
+              }
+
+              newTracks.push(trackId);
+              return;
+            });
+          }
+        },
+        (err) => {
+          throw err;
+        }
+      );
+
+      if (currSnapshotId !== snapshot_id) {
+        await spotifyApiClient.removeTracksFromPlaylist(
+          playlist.id,
+          tracksToRemove
+        );
+
+        await axiosInstace.patch(`/playlists/update/${playlist.id}`, {
+          snapshot_id: currSnapshotId,
+          tracks: newTracks,
+        });
+      }
+    }
+  },
+  {
+    scheduled: true,
+    timezone: "America/Sao_Paulo",
+  }
+);
+
+router.get("/", async (req: Request, res: Response) => {
+  res.status(200).send("The Guard Bot is on!");
 });
 
 app.listen(process.env.PORT);
