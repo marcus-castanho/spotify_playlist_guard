c4ca0c3500cd441b0dbe4f85b0cc53f0be09fd2d
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2022-09-24 13:53:21 -0300

    feat: add log when app starts

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,20 @@
-import { AppService } from "./modules";
+import { AppService } from './modules';
 
 (function bootstrap() {
-  AppService.getInstance()
-    .startAsyncModules()
-    .then((appInstance) => {
-      appInstance.startApp();
-    })
-    .catch((error) => {
-      console.log({
-        error: error.message,
-        message: "Failed to start the app. Retrying in 1 minute.",
-      });
-      setTimeout(() => {
-        bootstrap();
-      }, 60000);
-    });
+    AppService.getInstance()
+        .startAsyncModules()
+        .then((appInstance) => {
+            appInstance
+                .startApp()
+                .then(() => console.log('Application running.'));
+        })
+        .catch((error) => {
+            console.log({
+                error: error.message,
+                message: 'Failed to start the app. Retrying in 1 minute.',
+            });
+            setTimeout(() => {
+                bootstrap();
+            }, 60000);
+        });
 })();
