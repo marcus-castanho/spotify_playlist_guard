5da6c51f179c1678f3998e8c161b24053148fcee
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2023-03-19 13:14:18 -0300

    chor(docker): create dev env with docker compose and Dockerfile
    -Create Dockerfile for building an image of the app;
    -Create a main service for the app in dockercompose;
    -Update info on env vars;

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,7 @@ import { AppService } from './modules';
             logger('%j', {
                 error: error.message,
                 message: 'Failed to start the app. Retrying in 1 minute.',
+                stack: error.stack,
             });
             setTimeout(() => {
                 bootstrap();
