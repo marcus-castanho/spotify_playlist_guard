f4f3a624b30bc722cfa14edab62d35fa535fb7be
Marcus <65380139+marcus-castanho@users.noreply.github.com>
2024-01-26 14:46:15 -0300

    chore: change default port to 3000

--- src/modules/server/config/serverConfig.ts
+++ src/modules/server/config/serverConfig.ts
@@ -1,7 +1,7 @@
 import { ServerConfig } from '../@types';
 
 export const serverConfig: ServerConfig = {
-    port: process.env.PORT || '8080',
+    port: process.env.PORT || '3000',
     corsOptions: {
         origin: [process.env.APP_URL],
         methods: ['GET'],
